d9b0ccb017c2f60efdf6cc4fbb9371da7d62aeaf
dharmender0801 <teamsoul234@gmail.com>
2023-08-17 17:26:17 +0530

    Nigeria Billing

---
+++ Quiz2playNigeriaBilling/src/main/java/com/nigeria/Quiz2playNigeriaBillingApplication.java
@@ -0,0 +1,13 @@
+package com.nigeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz2playNigeriaBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz2playNigeriaBillingApplication.class, args);
+	}
+
+}
---
+++ Quiz2playNigeriaBilling/src/test/java/com/nigeria/Quiz2playNigeriaBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nigeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz2playNigeriaBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
