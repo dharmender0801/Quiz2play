08588d556ad42820810ffc5232f40e1fa5324cd3
dharmender0801 <teamsoul234@gmail.com>
2023-08-18 17:04:56 +0530

    Billing Gateway

---
+++ Quiz2playBillingGateway/src/main/java/com/kenya/Quiz2playBillingGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kenya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz2playBillingGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz2playBillingGatewayApplication.class, args);
+	}
+
+}
---
+++ Quiz2playBillingGateway/src/test/java/com/kenya/Quiz2playBillingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz2playBillingGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
